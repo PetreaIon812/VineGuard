eae20399036ee4607f2f206c3186c1aff76fe585
PetreaIon124 <petrea.natalia@yahoo.com>
2026-05-04 19:34:22 +0300

    Adaugat APK

--- VineGuardProject/src/app/build.gradle.kts
+++ VineGuardProject/src/app/build.gradle.kts
@@ -99,4 +99,9 @@ dependencies {
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
 
     implementation("com.microsoft.onnxruntime:onnxruntime-android:1.16.3")
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
